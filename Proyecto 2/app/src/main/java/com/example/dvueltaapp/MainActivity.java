5d8f2a2b74443781b4d35863027f306c42d0ccd3
DANIEL MORALA JIMÉNEZ <dmorala@REACCIONA.LOCAL>
2021-04-13 08:48:26 +0200

    TFG

---
+++ Proyecto 2/app/src/main/java/com/example/dvueltaapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.dvueltaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText userName;
+    private EditText userPassword;
+    private Button enviar;
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+}

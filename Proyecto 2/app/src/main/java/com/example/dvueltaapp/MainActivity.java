4b5247e46bb235cf81e0f59411c9e4393465661b
Daniel Morala <morala84@gmail.com>
2021-04-18 21:59:11 +0200

    Login Volley

--- Proyecto 2/app/src/main/java/com/example/dvueltaapp/MainActivity.java
+++ Proyecto 2/app/src/main/java/com/example/dvueltaapp/MainActivity.java
@@ -5,9 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
     private EditText name;
     private EditText password;
     private Button login;
+    private int mStatusCode = 0;
+    private final String url = "http://preskynet.dvuelta.es/api10getuserapikey";
+    private final String apiKeyAcess = "2c94243c0c0dc4452db4efd257d34d2f";
+    Intent intent;
+
+    String user;
+    String passwordUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +41,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        name = (EditText)findViewById(R.id.userName);
-        password = (EditText)findViewById(R.id.userPassword);
+        name = (EditText) findViewById(R.id.userName);
+        password = (EditText) findViewById(R.id.userPassword);
         login = (Button) findViewById(R.id.botonEntrar);
+        user = name.getText().toString();
+        passwordUser = password.getText().toString();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
-                //intent.putExtra("name", response.toString());
-                startActivity(intent);
+                user = name.getText().toString();
+                passwordUser = password.getText().toString();
+                postMethod();
             }
         });
+    }
+
+    public void postMethod() {
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response);
+                Log.e("LOG_VOLLEY: ", String.valueOf(mStatusCode));
+
+                if (mStatusCode == 200){
+                    intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                    intent.putExtra("name", response);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        Log.e("LOG_VOLLEY_DANI: ", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                mStatusCode = response.statusCode;
+                return super.parseNetworkResponse(response);
+            }
+
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("apiKey", apiKeyAcess);
+                params.put("data", "{\"user\":\"" + user + "\", \"password\":\"" + passwordUser + "\"}");
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(postRequest);
     }
 }
--- Proyecto 2/app/src/main/java/com/example/dvueltaapp/WelcomeActivity.java
+++ Proyecto 2/app/src/main/java/com/example/dvueltaapp/WelcomeActivity.java
@@ -20,49 +20,15 @@ import java.util.Map;
 public class WelcomeActivity extends AppCompatActivity {
 
     private TextView welcome;
-    private int mStatusCode = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         getSupportActionBar().hide();
-        //Bundle extras = getIntent().getExtras();
-        //String welcomeName = (String) extras.getString("name");
+        Bundle extras = getIntent().getExtras();
+        String welcomeName = (String) extras.getString("name");
         welcome = (TextView) findViewById(R.id.welcomeText);
-        postMethod();
-    }
-
-    public void postMethod() {
-        String url = "http://preskynet.dvuelta.es/api10getuserapikey";
-
-        StringRequest postRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                welcome.setText(response);
-                System.out.println(response);
-                Log.e("LOG_VOLLEY: ", String.valueOf(mStatusCode));
-            }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                        Log.e("LOG_VOLLEY_DANI: ", error.toString());
-                    }
-                }
-        ) {
-            @Override
-            protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                mStatusCode = response.statusCode;
-                return super.parseNetworkResponse(response);
-            }
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("apiKey", "2c94243c0c0dc4452db4efd257d34d2f");
-                params.put("data", "{\"user\":\"36712897K\", \"password\":\"oDe4z2aQ\"}");
-                return params;
-            }
-        };
-        Volley.newRequestQueue(this).add(postRequest);
+        welcome.setText(welcomeName);
     }
 }

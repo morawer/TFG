01d6ccffec8faf3bd4f2e468cdb1cafdd7c2c24d
DANIEL MORALA JIMÉNEZ <morala84@gmail.com>
2021-05-10 13:56:24 +0200

    Prueba generar pdf.

--- Proyecto 2/app/src/main/java/com/example/dvueltaapp/ImagenExpActivity.java
+++ Proyecto 2/app/src/main/java/com/example/dvueltaapp/ImagenExpActivity.java
@@ -1,11 +1,9 @@
 package com.example.dvueltaapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
@@ -15,10 +13,16 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.GridView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
@@ -26,23 +30,32 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.HeaderFooter;
+import com.lowagie.text.Image;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.ColumnText;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+
+import harmony.java.awt.Color;
 
 public class ImagenExpActivity extends AppCompatActivity {
 
@@ -50,6 +63,7 @@ public class ImagenExpActivity extends AppCompatActivity {
     private final String URL = "http://preskynet.dvuelta.es/api10getexpedientsimage";
 
     private final static String NOMBRE_DIRECTORIO = "DVuelta";
+    private String nombrePdf;
 
     String apiKeyUser, idImagen, nomOrg, hechoDenunciado, matricula, puntos, fechaExp, estadoExp, numExp;
     Imagenes imagenes;
@@ -58,8 +72,7 @@ public class ImagenExpActivity extends AppCompatActivity {
     int statusCode = 0;
     TextView nomOrgExpText, hechoExpText, matriculaExpText, puntosExpText, fechaExpText, estadoExpText, numExpText;
     ImageView imgDecode = null;
-    ImageView baseImagen;
-    GridView gridViewImagenes;
+    Button descargarPDF;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -104,19 +117,19 @@ public class ImagenExpActivity extends AppCompatActivity {
         matricula = (String) extras.getString("matricula");
         puntos = (String) extras.getString("puntos");
         fechaExp = (String) extras.getString("fecha");
-        estadoExp  = (String) extras.getString("estado");
+        estadoExp = (String) extras.getString("estado");
         numExp = (String) extras.getString("numExp");
 
         imagenes.setApiKeyUser(apiKeyUser);
         imagenes.setIdImagen(idImagen);
 
-        nomOrgExpText = (TextView)findViewById(R.id.nomOrgExp);
-        hechoExpText = (TextView)findViewById(R.id.hechoExp);
-        matriculaExpText = (TextView)findViewById(R.id.matriculaExp);
-        puntosExpText = (TextView)findViewById(R.id.puntosExp);
-        fechaExpText = (TextView)findViewById(R.id.fechaExp);
-        estadoExpText = (TextView)findViewById(R.id.expedienteEstadoExp);
-        numExpText = (TextView)findViewById(R.id.numExpImg);
+        nomOrgExpText = (TextView) findViewById(R.id.nomOrgExp);
+        hechoExpText = (TextView) findViewById(R.id.hechoExp);
+        matriculaExpText = (TextView) findViewById(R.id.matriculaExp);
+        puntosExpText = (TextView) findViewById(R.id.puntosExp);
+        fechaExpText = (TextView) findViewById(R.id.fechaExp);
+        estadoExpText = (TextView) findViewById(R.id.expedienteEstadoExp);
+        numExpText = (TextView) findViewById(R.id.numExpImg);
 
         nomOrgExpText.setText("Lugar: " + nomOrg);
         hechoExpText.setText("Denuncia: " + hechoDenunciado);
@@ -129,7 +142,26 @@ public class ImagenExpActivity extends AppCompatActivity {
         numExpText.setTypeface(typeface);
         numExpText.setText("Expediente: " + numExp);
 
+        descargarPDF = (Button) findViewById(R.id.botonPDF);
+
         postMethod();
+
+        // Permisos.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,}, 1000);
+        } else {
+        }
+
+
+        descargarPDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crearPDF();
+                Toast.makeText(ImagenExpActivity.this, "Se creo el PDF", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
     public void postMethod() {
@@ -146,7 +178,6 @@ public class ImagenExpActivity extends AppCompatActivity {
                         startActivity(intent);
                     } else {
                         leerJson(response);
-                        generarPdf(imagenes);
                     }
                 }
             }
@@ -219,66 +250,124 @@ public class ImagenExpActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
             Log.d("TAG-leerJson()_ERROR", "Error de lectura Json.");
-        }    }
-
-    public void generarPdf(Imagenes imagen){
-
-        Document documento = new Document();
-
-        try {
-            Calendar c = Calendar.getInstance();
-            SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-            String fecha = df.format(c.getTime());
-
-            File f = crearFichero(fecha);
-
-            FileOutputStream ficheroPdf = new FileOutputStream(f.getAbsolutePath());
-
-            PdfWriter writer = PdfWriter.getInstance(documento, ficheroPdf);
-
-            for (int i = 0; i < imagen.getImagenesBase64().size(); i++) {
-                documento.add((Element) decodeImage(imagen.getImagenesBase64().get(i).getBase64()));
-
-
-            }
-
-
-        } catch (DocumentException e) {
-            Log.e("ERROR-- generarPdf()", e.getMessage());
-        } catch (IOException e) {
-            Log.e("ERROR-- generarPdf()", e.getMessage());
-        } finally {
-            documento.close();
         }
     }
 
-    public static File crearFichero(String nombreFichero) throws IOException {
+    public void crearPDF() {
+        // Creamos el documento.
+        Document documento = new Document();
 
+        try {
+
+            File f = crearFichero(nombrePdf);
+            FileOutputStream ficheroPdf = new FileOutputStream(f.getAbsolutePath());
+            // Creamos el flujo de datos de salida para el fichero donde
+            // guardaremos el pdf.
+
+
+            // Asociamos el flujo que acabamos de crear al documento.
+            PdfWriter writer = PdfWriter.getInstance(documento, ficheroPdf);
+
+            // Incluimos el pie de pagina y una cabecera
+            HeaderFooter cabecera = new HeaderFooter(new Phrase(
+                    "Esta es mi cabecera"), false);
+            HeaderFooter pie = new HeaderFooter(new Phrase(
+                    "Este es mi pie de pagina"), false);
+
+            documento.setHeader(cabecera);
+            documento.setFooter(pie);
+
+            // Abrimos el documento.
+            documento.open();
+
+            // Añadimos un titulo con la fuente por defecto.
+            documento.add(new Paragraph("Titulo 1"));
+
+            Font font = FontFactory.getFont(FontFactory.HELVETICA, 28,
+                    Font.BOLD, Color.RED);
+            documento.add(new Paragraph("Titulo personalizado", font));
+
+            // Insertamos una imagen que se encuentra en los recursos de la
+            // aplicacion.
+            Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),
+                    R.drawable.logo);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            Image imagen = Image.getInstance(stream.toByteArray());
+            documento.add(imagen);
+
+            // Insertamos una tabla.
+            PdfPTable tabla = new PdfPTable(5);
+            for (int i = 0; i < 15; i++) {
+                tabla.addCell("Celda " + i);
+            }
+            documento.add(tabla);
+
+            // Agregar marca de agua
+            font = FontFactory.getFont(FontFactory.HELVETICA, 42, Font.BOLD,
+                    Color.GRAY);
+            ColumnText.showTextAligned(writer.getDirectContentUnder(),
+                    Element.ALIGN_CENTER, new Paragraph(
+                            "androfast.com", font), 297.5f, 421,
+                    writer.getPageNumber() % 2 == 1 ? 45 : -45);
+
+        } catch (DocumentException e) {
+
+            Log.e("ERROR", e.getMessage());
+
+        } catch (IOException e) {
+
+            Log.e("ERROR", e.getMessage());
+
+        } finally {
+            // Cerramos el documento.
+            documento.close();
+        }
+
+    }
+
+    public static File crearFichero(String nombreFichero) throws IOException {
         File ruta = getRuta();
         File fichero = null;
-        if (ruta != null)
-            fichero = new File (ruta, nombreFichero);
+        if (ruta != null) {
+            fichero = new File(ruta, nombreFichero);
+        }
+
         return fichero;
     }
 
+    /**
+     * Obtenemos la ruta donde vamos a almacenar el fichero.
+     *
+     * @return
+     */
     public static File getRuta() {
+
+        // El fichero sera almacenado en un directorio dentro del directorio
+        // Descargas
         File ruta = null;
-        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
-            ruta = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), NOMBRE_DIRECTORIO);
+        if (Environment.MEDIA_MOUNTED.equals(Environment
+                .getExternalStorageState())) {
+            ruta = new File(
+                    Environment
+                            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                    NOMBRE_DIRECTORIO);
 
             if (ruta != null) {
-                if(!ruta.mkdirs()) {
-                    if(!ruta.exists()) {
+                if (!ruta.mkdirs()) {
+                    if (!ruta.exists()) {
                         return null;
                     }
                 }
             }
         } else {
         }
+
         return ruta;
     }
 
-    public ImageView decodeImage(String base64){
+
+    public ImageView decodeImage(String base64) {
         byte[] imageBytes = Base64.decode(base64, Base64.DEFAULT);
         Bitmap decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
         imgDecode.setImageBitmap(decodedImage);

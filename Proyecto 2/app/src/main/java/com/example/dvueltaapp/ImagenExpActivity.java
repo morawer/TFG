9372393f8d662bfb55ce36a013c4e227e2a86b02
DANIEL MORALA JIMÉNEZ <morala84@gmail.com>
2021-05-11 10:19:18 +0200

    crearPDF() en pruebas

--- Proyecto 2/app/src/main/java/com/example/dvueltaapp/ImagenExpActivity.java
+++ Proyecto 2/app/src/main/java/com/example/dvueltaapp/ImagenExpActivity.java
@@ -6,7 +6,9 @@ import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Base64;
@@ -30,32 +32,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Element;
-import com.lowagie.text.Font;
-import com.lowagie.text.FontFactory;
-import com.lowagie.text.HeaderFooter;
-import com.lowagie.text.Image;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.Phrase;
-import com.lowagie.text.pdf.ColumnText;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfWriter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
-import harmony.java.awt.Color;
 
 public class ImagenExpActivity extends AppCompatActivity {
 
@@ -146,18 +132,11 @@ public class ImagenExpActivity extends AppCompatActivity {
 
         postMethod();
 
-        // Permisos.
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,}, 1000);
-        } else {
-        }
-
 
         descargarPDF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 crearPDF();
-                Toast.makeText(ImagenExpActivity.this, "Se creo el PDF", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -254,116 +233,31 @@ public class ImagenExpActivity extends AppCompatActivity {
     }
 
     public void crearPDF() {
-        // Creamos el documento.
-        Document documento = new Document();
+
+        ActivityCompat.requestPermissions(ImagenExpActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+
+        PdfDocument myPdfDocument = new PdfDocument();
+        PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(300, 600, 1).create();
+        PdfDocument.Page myPage = myPdfDocument.startPage(myPageInfo);
+
+        Paint myPaint = new Paint();
+        String myString = "Holaaaaa Danielo!!";
+        myPage.getCanvas().drawText(myString, 10, 25, myPaint);
+        myPdfDocument.finishPage(myPage);
 
         try {
-
-            File f = crearFichero(nombrePdf);
-            FileOutputStream ficheroPdf = new FileOutputStream(f.getAbsolutePath());
-            // Creamos el flujo de datos de salida para el fichero donde
-            // guardaremos el pdf.
-
-
-            // Asociamos el flujo que acabamos de crear al documento.
-            PdfWriter writer = PdfWriter.getInstance(documento, ficheroPdf);
-
-            // Incluimos el pie de pagina y una cabecera
-            HeaderFooter cabecera = new HeaderFooter(new Phrase(
-                    "Esta es mi cabecera"), false);
-            HeaderFooter pie = new HeaderFooter(new Phrase(
-                    "Este es mi pie de pagina"), false);
-
-            documento.setHeader(cabecera);
-            documento.setFooter(pie);
-
-            // Abrimos el documento.
-            documento.open();
-
-            // Añadimos un titulo con la fuente por defecto.
-            documento.add(new Paragraph("Titulo 1"));
-
-            Font font = FontFactory.getFont(FontFactory.HELVETICA, 28,
-                    Font.BOLD, Color.RED);
-            documento.add(new Paragraph("Titulo personalizado", font));
-
-            // Insertamos una imagen que se encuentra en los recursos de la
-            // aplicacion.
-            Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),
-                    R.drawable.logo);
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-            Image imagen = Image.getInstance(stream.toByteArray());
-            documento.add(imagen);
-
-            // Insertamos una tabla.
-            PdfPTable tabla = new PdfPTable(5);
-            for (int i = 0; i < 15; i++) {
-                tabla.addCell("Celda " + i);
+        File myFile = new File(Environment.getExternalStorageDirectory(), "/Download/" + numExp + ".pdf");
+            if (!myFile.exists()) {
+                myFile.createNewFile();
             }
-            documento.add(tabla);
-
-            // Agregar marca de agua
-            font = FontFactory.getFont(FontFactory.HELVETICA, 42, Font.BOLD,
-                    Color.GRAY);
-            ColumnText.showTextAligned(writer.getDirectContentUnder(),
-                    Element.ALIGN_CENTER, new Paragraph(
-                            "androfast.com", font), 297.5f, 421,
-                    writer.getPageNumber() % 2 == 1 ? 45 : -45);
-
-        } catch (DocumentException e) {
-
-            Log.e("ERROR", e.getMessage());
-
-        } catch (IOException e) {
-
-            Log.e("ERROR", e.getMessage());
-
-        } finally {
-            // Cerramos el documento.
-            documento.close();
+            myPdfDocument.writeTo(new FileOutputStream(myFile));
+            Toast.makeText(this, "PDF creado." + myFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "ERROR!!", Toast.LENGTH_SHORT).show();
         }
 
-    }
-
-    public static File crearFichero(String nombreFichero) throws IOException {
-        File ruta = getRuta();
-        File fichero = null;
-        if (ruta != null) {
-            fichero = new File(ruta, nombreFichero);
-        }
-
-        return fichero;
-    }
-
-    /**
-     * Obtenemos la ruta donde vamos a almacenar el fichero.
-     *
-     * @return
-     */
-    public static File getRuta() {
-
-        // El fichero sera almacenado en un directorio dentro del directorio
-        // Descargas
-        File ruta = null;
-        if (Environment.MEDIA_MOUNTED.equals(Environment
-                .getExternalStorageState())) {
-            ruta = new File(
-                    Environment
-                            .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-                    NOMBRE_DIRECTORIO);
-
-            if (ruta != null) {
-                if (!ruta.mkdirs()) {
-                    if (!ruta.exists()) {
-                        return null;
-                    }
-                }
-            }
-        } else {
-        }
-
-        return ruta;
+        myPdfDocument.close();
     }
 
 

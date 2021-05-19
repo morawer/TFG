64211fda5afd78d38088376187ec75c8aac0dc15
DANIEL MORALA JIMÉNEZ <morala84@gmail.com>
2021-05-19 13:40:11 +0200

    Pruebas con camara y compresion de imagen

--- Proyecto 2/app/src/main/java/com/example/dvueltaapp/EnvioExpActivity.java
+++ Proyecto 2/app/src/main/java/com/example/dvueltaapp/EnvioExpActivity.java
@@ -1,19 +1,36 @@
 package com.example.dvueltaapp;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class EnvioExpActivity extends AppCompatActivity {
 
     Button camaraButton, galeriaButton;
     ImageView imgView;
+
+    private static final String TAG = "MainActivity";
+    private static final int PICTURE_RESULT = 122 ;
+    private ContentValues values;
+    private Uri imageUri;
+    private Bitmap thumbnail;
+    String imageurl;
 
 
     @Override
@@ -21,33 +38,74 @@ public class EnvioExpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_envio_exp);
 
-        camaraButton = findViewById(R.id.botonCamara);
-        galeriaButton = findViewById(R.id.botonGaleria);
+
+
         imgView = findViewById(R.id.imagenCamara);
-
-
+        camaraButton = findViewById(R.id.botonCamara);
         camaraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                abrirCamara();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    //Check permissions for Android 6.0+
+                    if(!checkExternalStoragePermission()){
+                        return;
+                    }
+                }
+                values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, "MyPicture");
+                values.put(MediaStore.Images.Media.DESCRIPTION, "Photo taken on " + System.currentTimeMillis());
+                imageUri = getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                String imagenOriginal = MediaStore.ACTION_IMAGE_CAPTURE;
+
+                Bitmap imageScaled = Bitmap.createScaledBitmap(imagenOriginal, 600, 800, false);
+                Intent intent = new Intent(imageScaled);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, PICTURE_RESULT);
             }
         });
-    }
 
-    private void abrirCamara() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, 1);
-        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imgBitmap = (Bitmap) extras.get("data");
-            imgView.setImageBitmap(imgBitmap);
+        switch (requestCode) {
+            case PICTURE_RESULT:
+                if (requestCode == PICTURE_RESULT)
+                    if (resultCode == Activity.RESULT_OK) {
+                        try {
+                            thumbnail = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                            imgView.setImageBitmap(thumbnail);
+                            imageurl = getRealPathFromURI(imageUri);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
         }
     }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+    }
+
+    private boolean checkExternalStoragePermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission not granted.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 225);
+        } else {
+            Log.i(TAG, "You already have permission!");
+            return true;
+        }
+
+        return false;
+    }
+
 
 }
